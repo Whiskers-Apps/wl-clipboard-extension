7d104be5b3352533882489e2f23b23d8acf7743a
lighttigerxiv <lighttigerxiv@gmail.com>
2024-10-23 15:49:11 +0100

    clean code

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -1,7 +1,4 @@
-use std::{
-    env,
-    process::{exit, Command},
-};
+use std::process::exit;
 
 use whiskers_launcher_core::features::{
     core::extensions::get_form_response, extensions::ExtensionRequest,
@@ -39,134 +36,85 @@ pub fn on_run_commands(request: ExtensionRequest) {
         exit(0)
     }
 
-    if command == "copy-image" {
-        if is_wayland() {
-            let args = request.args;
-            let id: usize = args.get(0).unwrap().parse().unwrap();
-            let clip: ImageClip = clipboard
-                .image_clips
-                .iter()
-                .find(|c| c.id == id)
-                .unwrap()
-                .to_owned();
+    if command == "edit-text-clip" {
+        let response = get_form_response();
 
-            let clip_path_str = clip.path.into_os_string().into_string().unwrap();
+        let name = response.get_result("name").unwrap().field_value;
+        let text = response.get_result("text").unwrap().field_value;
+        let args = response.args;
+        let id: usize = args.get(0).unwrap().parse().unwrap();
+        let mut new_text_clips = Vec::<TextClip>::new();
 
-            if is_wayland() {
-                #[cfg(target_os = "linux")]
-                {
-                    Command::new("sh")
-                        .arg("-c")
-                        .arg(format!("cat {} | wl-copy --type image/png", &clip_path_str))
-                        .spawn()
-                        .expect("Error copying image");
-                }
-            }
-
-            #[cfg(target_os = "windows")]
-            {
-                let script = r#"
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.Clipboard]::SetImage([System.Drawing.Image]::FromFile("%path%"))"#
-                    .replace("%path%", &clip_path_str);
-
-                powershell_script::run(&script).expect("Error executing copy command");
-            }
-
-            exit(0)
+        for clip in clipboard.clone().text_clips {
+            new_text_clips.push(if clip.id == id {
+                clip.to_owned().set_name(&name).set_text(&text)
+            } else {
+                clip
+            });
         }
 
-        if command == "edit-text-clip" {
-            let response = get_form_response();
+        let mut clipboard_mod = clipboard.clone();
+        clipboard_mod.text_clips = new_text_clips;
 
-            let name = response.get_result("name").unwrap().field_value;
-            let text = response.get_result("text").unwrap().field_value;
-            let args = response.args;
-            let id: usize = args.get(0).unwrap().parse().unwrap();
-            let mut new_text_clips = Vec::<TextClip>::new();
+        write_clipboard(&clipboard_mod);
 
-            for clip in clipboard.clone().text_clips {
-                new_text_clips.push(if clip.id == id {
-                    clip.to_owned().set_name(&name).set_text(&text)
-                } else {
-                    clip
-                });
-            }
-
-            let mut clipboard_mod = clipboard.clone();
-            clipboard_mod.text_clips = new_text_clips;
-
-            write_clipboard(&clipboard_mod);
-
-            exit(0)
-        }
-
-        if command == "delete-text-clip" {
-            let args = request.args;
-            let id: usize = args.get(0).unwrap().parse().unwrap();
-            let mut clipboard_mod = clipboard.clone();
-            clipboard_mod.text_clips = clipboard
-                .text_clips
-                .iter()
-                .map(|clip| clip.to_owned())
-                .filter(|clip| clip.id != id)
-                .collect();
-
-            write_clipboard(&clipboard_mod);
-
-            exit(0)
-        }
-
-        if command == "edit-image-clip" {
-            let response = get_form_response();
-
-            let name = response.get_result("name").unwrap().field_value;
-            let path = response.get_result("path").unwrap().field_value;
-            let args = response.args;
-            let id: usize = args.get(0).unwrap().parse().unwrap();
-            let mut image_clips_mod = Vec::<ImageClip>::new();
-
-            for clip in clipboard.clone().image_clips {
-                image_clips_mod.push(if clip.id == id {
-                    clip.to_owned().set_name(&name).set_path(&path)
-                } else {
-                    clip
-                });
-            }
-
-            let mut clipboard_mod = clipboard.clone();
-            clipboard_mod.image_clips = image_clips_mod;
-
-            write_clipboard(&clipboard_mod);
-
-            exit(0)
-        }
-
-        if command == "delete-image-clip" {
-            let args = request.args;
-            let id: usize = args.get(0).unwrap().parse().unwrap();
-            let mut clipboard_mod = clipboard.clone();
-            clipboard_mod.image_clips = clipboard
-                .image_clips
-                .iter()
-                .map(|clip| clip.to_owned())
-                .filter(|clip| clip.id != id)
-                .collect();
-
-            write_clipboard(&clipboard_mod);
-
-            exit(0)
-        }
+        exit(0)
     }
 
-    pub fn is_wayland() -> bool {
-        if env::consts::OS != "linux" {
-            return false;
+    if command == "delete-text-clip" {
+        let args = request.args;
+        let id: usize = args.get(0).unwrap().parse().unwrap();
+        let mut clipboard_mod = clipboard.clone();
+        clipboard_mod.text_clips = clipboard
+            .text_clips
+            .iter()
+            .map(|clip| clip.to_owned())
+            .filter(|clip| clip.id != id)
+            .collect();
+
+        write_clipboard(&clipboard_mod);
+
+        exit(0)
+    }
+
+    if command == "edit-image-clip" {
+        let response = get_form_response();
+
+        let name = response.get_result("name").unwrap().field_value;
+        let path = response.get_result("path").unwrap().field_value;
+        let args = response.args;
+        let id: usize = args.get(0).unwrap().parse().unwrap();
+        let mut image_clips_mod = Vec::<ImageClip>::new();
+
+        for clip in clipboard.clone().image_clips {
+            image_clips_mod.push(if clip.id == id {
+                clip.to_owned().set_name(&name).set_path(&path)
+            } else {
+                clip
+            });
         }
 
-        match env::var("XDG_SESSION_TYPE") {
-            Ok(session) => &session.to_lowercase() == "wayland",
-            Err(_) => false,
-        }
+        let mut clipboard_mod = clipboard.clone();
+        clipboard_mod.image_clips = image_clips_mod;
+
+        write_clipboard(&clipboard_mod);
+
+        exit(0)
+    }
+
+    if command == "delete-image-clip" {
+        let args = request.args;
+        let id: usize = args.get(0).unwrap().parse().unwrap();
+        let mut clipboard_mod = clipboard.clone();
+        clipboard_mod.image_clips = clipboard
+            .image_clips
+            .iter()
+            .map(|clip| clip.to_owned())
+            .filter(|clip| clip.id != id)
+            .collect();
+
+        write_clipboard(&clipboard_mod);
+
+        exit(0)
     }
 }
